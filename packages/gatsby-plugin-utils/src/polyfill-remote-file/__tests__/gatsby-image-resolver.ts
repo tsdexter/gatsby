43e9669faf33c322fa2f19ae4defb087ab5de79b
veryspry <ehlinger.matt@gmail.com>
2022-05-26 15:09:10 +0200

    test: get tests working for image cdn source url tracking

--- packages/gatsby-plugin-utils/src/polyfill-remote-file/__tests__/gatsby-image-resolver.ts
+++ packages/gatsby-plugin-utils/src/polyfill-remote-file/__tests__/gatsby-image-resolver.ts
@@ -736,9 +736,7 @@ describe(`gatsbyImageData`, () => {
     )
     const actions = {
       addGatsbyImageSourceUrl: jest.fn(),
-    } as Actions
-
-    process.env.GATSBY_SHOULD_TRACK_IMAGE_CDN_URLS = `1`
+    } as unknown as Actions
 
     await gatsbyImageResolver(
       portraitSource,
@@ -749,9 +747,7 @@ describe(`gatsbyImageData`, () => {
       actions
     )
 
-    process.env.GATSBY_SHOULD_TRACK_IMAGE_CDN_URLS = null
-
-    expect(actions.addGatsbyImageSourceUrl.mock.calls[0][0]).toEqual(
+    expect(actions.addGatsbyImageSourceUrl).toHaveBeenCalledWith(
       portraitSource.url
     )
   })
