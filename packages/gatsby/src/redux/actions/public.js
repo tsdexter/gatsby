2cf464a9e7f70766589ab2daaf636a2dc3390584
veryspry <ehlinger.matt@gmail.com>
2022-05-26 17:51:48 +0200

    fix: remove duplicate action from public

--- packages/gatsby/src/redux/actions/public.js
+++ packages/gatsby/src/redux/actions/public.js
@@ -1259,12 +1259,6 @@ actions.addGatsbyImageSourceUrl = (sourceUrl: string) => {
   }
 }
 
-actions.clearGatsbyImageSourceUrls = () => {
-  return {
-    type: `CLEAR_GATSBY_IMAGE_SOURCE_URL`,
-  }
-}
-
 /**
  * DEPRECATED. Use createJobV2 instead.
  *
